aa42f73091d124cca3a43dc62c40b5363b0d1965
sairam-java <sairam10u@gmail.com>
2022-07-17 14:05:52 -0700

    chged numc to numd in master

--- sum.py
+++ sum.py
@@ -1,9 +1,9 @@
 
 numa = 1.1
 numb = 2.2
-numc = 3.3
+numd = 3.3
 
-sum = numa + numb + numc
-multi = numa*numb*numc
+sum = numa + numb + numd
+multi = numa*numb*numd
 
 print("Sum is", sum, "multiplication is", multi)
